f19da4eaed151892b4e795d5ccf40b73da8feccd
muhammadfarras <maruffarras@gmail.com>
2022-03-02 15:11:22 +0700

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.farras.sdit.perpussditanakshalihbogorabsensi {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.farras.sdit.perpussditanakshalihbogorabsensi to javafx.fxml;
+    exports com.farras.sdit.perpussditanakshalihbogorabsensi;
+    exports com.farras.sdit.perpussditanakshalihbogorabsensi.controller;
+    opens com.farras.sdit.perpussditanakshalihbogorabsensi.controller to javafx.fxml;
+}
